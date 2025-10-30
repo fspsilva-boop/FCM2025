212d37e2e6e437528526784731d5d6d0d2afc349
fspsilva-boop <fsp.silva@unesp.br>
2025-10-30 09:46:14 -0300

    Dados Dengue DATASUS

--- PraticasR/Data_sus.R
+++ PraticasR/Data_sus.R
@@ -72,3 +72,62 @@ ggplot(df_final)+
 ggplot(df_final)+
   geom_boxplot(aes(x = factor(Ano), y = incidencia))
 
+### Lendo dados DBC
+
+library(tidyverse)
+
+devtools::install_github("danicat/read.dbc")
+
+install.packages("read.dbc")
+
+dados <- read.dbc::read.dbc("DENGBR24.dbc")
+
+pkgbuild::check_build_tools(debug = TRUE)
+
+devtools::install_github("danicat/read.dbc")
+
+Sys.which("make")
+
+writeLines('PATH="${RTOOLS45_HOME}\\usr\\bin;${PATH}"', con = "~/.Renviron")
+
+dados <- read.dbc::read.dbc("DENGBR24.dbc")
+
+Sys.which("make")
+install.packages("read.dbc")
+devtools::install_github("danicat/read.dbc")
+
+dados <- read.dbc::read.dbc("DENGBR24.dbc")
+
+dados_sp <- dados %>%
+  filter(grepl("^35", ID_MN_RESI))
+
+write.csv(dados_sp, "dados_dengue_sp.csv")
+
+library(tidyverse)
+library(duckdb)
+
+con <- dbConnect(duckdb::duckdb(), dbdir = "meubanco.duckdb")
+
+dbExecute(con, "CREATE TABLE dados AS SELECT * FROM read_csv_auto('dados_dengue_sp.csv')")
+
+dbWriteTable(con, "dados", dados_sp, overwrite = TRUE)
+
+# Consultas SQL
+dbGetQuery(con, "DESCRIBE dados")
+
+sem_pri <- dbGetQuery(con, 'SELECT SEM_PRI FROM dados')
+df_sem_prim <- data.frame(sem_pri)
+
+df_sem_prim %>%
+  group_by(SEM_PRI) %>%
+  summarise(
+    n = n()
+  ) %>%
+  ungroup() %>%
+  ggplot()+
+  geom_col(aes(x = SEM_PRI, y = n))+
+  theme(
+    axis.text.x = element_text(angle = 90, hjust = 1.0, vjust = 0.5)
+  )
+  
+
